ea69d72a2ed70e773bfdb88c47465dce58acbf84
Michielegante <garciacalvoluisfernando@gmail.com>
2024-12-28 14:06:32 -0600

    crear las clases model, mapeo clases a tablas con JPA, integrando diseño y vistas

--- src/main/java/com/curso/ecommerce/SpringEcommerceApplication.java
+++ src/main/java/com/curso/ecommerce/SpringEcommerceApplication.java
@@ -3,9 +3,9 @@ package com.curso.ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.SecurityDataConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+
+@SpringBootApplication
 public class SpringEcommerceApplication {
 
 	public static void main(String[] args) {
